916924b68d613cfab7e4c9f80fc2ca8988aaac7d
Brian Soto <kungfunub@gmail.com>
2015-03-26 17:54:38 -0700

    Updated a few features on where files get called

--- theme/templates/tripal_file_generic.tpl.php
+++ theme/templates/tripal_file_generic.tpl.php
@@ -1,5 +1,22 @@
-<?php
-//$organism = $variables['node']->organism;
 
+<?php
+$incomingNode = $variables['node'];
+$filesToAdd = $variables['files'];
+
+dpm($filesToAdd);
+
+if(is_array($filesToAdd)) {
+	foreach($filesToAdd as $file) {
+		print "$file->uri ";
+		print "$file->description";
+	} 
+} 
+else { ?>
+<div>I have the file, but I have no way of displaying it... </div>
+<?php 
+dpm($filesToAdd->uri);	
+drupal_set_message("weeee");
+	 print tripal_set_message( $filesToAdd->uri);
+	 print tripal_set_message(  $filesToAdd->description) ;		
+	 }
 ?>
-<p>barnicles.</p>
