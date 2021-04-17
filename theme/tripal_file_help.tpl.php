a791bd5dfc428d35a8a21a81ebbfb0df2835a276
dsenalik <dsenalik@wisc.edu>
2021-04-17 10:22:58 -0500

    Add optional file name column, admin interface, and help

---
+++ theme/tripal_file_help.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This template displays the help page for tripal file
+ */
+?>
+
+<h3>Module Description</h3>
+<p>The Tripal File module supports association of data files with content in a Chado database
+   and for integration of those associations with content types on a Tripal website.</p>
+
+<p>These associations are meant to support FAIR data principles by integrating with
+   Tripal content web services such that:<ul>
+
+    <li>Files are findable and accessible via Tripal's content web services.</li>
+    <li>Metadata about files use globally unique controlled vocabularies.
+        These metadata can be assigned as properties of each file.</li>
+    <li>License and usage details can be assigned to each file.</li>
+    </ul></p>
+
+<p>This module provides two new content types for Tripal sites: File and License.</p>
+
+<p>Development of the Tripal File module was funded by the
+   <a href="https://nsf.gov/awardsearch/showAward?AWD_ID=1659300">National Science Foundation award #1659300</a>
+   and the 
+   <a href="https://www.nrsp10.org/">National Research Support Program (NRSP) 10 project</a>
+   and the McIntire-Stennis project 1019284.</p>
+
+<p>Please see the <a href="https://tripal-file.readthedocs.io/en/latest/">online documentation</a>
+   for installation and usage instructions.</p>
